50f46469377e17de9b119aa7214f86be7320e987
doculigent <info@baraklabs.com>
2026-08-12 15:01:18 +0530

    alt mac build

---
+++ build/afterPack.cjs
@@ -0,0 +1,8 @@
+const { execFileSync } = require("node:child_process");
+const path = require("node:path");
+
+module.exports = async function afterPack(context) {
+  if (context.electronPlatformName !== "darwin") return;
+  const appPath = path.join(context.appOutDir, `${context.packager.appInfo.productFilename}.app`);
+  execFileSync("codesign", ["--force", "--deep", "--sign", "-", appPath], { stdio: "inherit" });
+};
